9c9b45ba54388b3c2bd3a4dd2aaeae35e7d052bd
Joel Peralta <peraltajoel88@gmail.com>
2024-08-12 02:37:44 -0400

    remove MenuIcon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,9 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
 const Navbar = () => {
   return (
     <AppBar position="static" sx={{ backgroundColor: "#f78f1e" }}>
       <Toolbar>
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Cursos
         </Typography>
